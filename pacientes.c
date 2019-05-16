2960454fd8cd8707a5d30826fe1070b407ec69be
Fernanda Moraes Costa <ferchan@hotmail.com.br>
2019-05-16 10:29:43 -0300

    criacao dos metodos e menu

--- pacientes.c
+++ pacientes.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+typedef struct paciente{
+    char *nome;
+    int telefone;
+    int grauUrgencia;
+} Paciente;
+
+void cadastrarPaciente(){}
+Paciente* buscarPaciente(){}
+Paciente* proximoPaciente(){}
+int verificaTamanhoFila(){}
+void removerPaciente(){}
+
+void main(){
+
+    int opcao;
+    do{
+        printf("\n*************************************");
+        printf("\n*                                   *");
+        printf("\n*   1. CADASTRAR PACIENTE           *");
+        printf("\n*   2. BUSCAR PACIENTE              *");
+        printf("\n*   3. PROXIMO PACIENTE             *");
+        printf("\n*   4. VERIFICAR TAMANHO DA LISTA   *");
+        printf("\n*   0. SAIR                         *");
+        printf("\n*                                   *");
+        printf("\n*************************************\n");
+        scanf("%d", &opcao);
+    } while(opcao != 0 || opcao < 0 || opcao > 4);
+
+
+}
